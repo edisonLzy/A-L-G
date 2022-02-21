2054c2130fcc81cf4c5fba71f2d626e526a8315c
evanzyli <45064281+edisonLzy@users.noreply.github.com>
2022-02-21 21:59:52 +0800

    feat: finish binary

---
+++ 新手/第三节课/binary.js
@@ -0,0 +1,16 @@
+function binary(arr, num) {
+    if (arr === null || arr.length === 0) return false;
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((right - left) / 2);
+        if (arr[mid] === num) return true
+        if (arr[mid] < num) {
+            // 说明 num 在 mid的右边
+            left = mid + 1
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false
+}
